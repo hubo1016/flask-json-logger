e2d7fcc126e649dcbc74e114620f20745a45305a
hubo <hubo@megvii.com>
2019-03-19 13:31:17 +0800

    python 2.7 compatibility

--- test.py
+++ test.py
@@ -8,14 +8,15 @@ if __name__ == '__main__':
     import os
     import signal
     os.environ['FLASK_APP'] = 'example.py'
-    with Popen(["python", "-m", "flask", "run", "-h", "127.0.0.1", "-p", "8999"],
-               shell=False) as p:
-        try:
-            r = requests.get('http://127.0.0.1:8999/')
-            r.raise_for_status()
-            assert r.text == "hello world"
-        finally:
-            p.terminate()
+    p = Popen(["python", "-m", "flask", "run", "-h", "127.0.0.1", "-p", "8999"],
+               shell=False)
+    try:
+        r = requests.get('http://127.0.0.1:8999/')
+        r.raise_for_status()
+        assert r.text == "hello world"
+    finally:
+        p.terminate()
+        p.wait()
     with open('test.log', 'r') as f:
         data = json.load(f)
         print(data)
